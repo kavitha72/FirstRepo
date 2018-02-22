da8f9142bf201fbdb5900970d3f8e50bc3288106
Rose <Rincy_Rose@Dell.com>
2018-02-22 14:38:18 +0530

    New Changes

--- AngularJS/AngularJS/script.js
+++ AngularJS/AngularJS/script.js
@@ -1,4 +1,12 @@
 ﻿var app = angular.module('myAngularApp', []);
-app.controller("myController", function ($scope) {
-    $scope.message = "Hello World"
+app.controller("firstController", function ($scope) {
+    $scope.message = "This is first controller";
+    $scope.showMsg = function (msg) {
+        alert(msg);
+    };
+});
+    app.controller("secondController", function ($scope) {
+        $scope.student = "Hello World!";
+       
+    
 });
---
+++ AngularJS/AngularJS/script1.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("myModule", []);
+app.controller("parentController", function ($scope, $rootScope) {
+    $scope.controllerName = "This is parentController";
+    $rootScope.message = "Hello World!";
+});
+app.controller("childController", function ($scope) {
+    $scope.controllerName = "childController";
+});
+app.controller('siblingController', function ($scope) {
+
+});
